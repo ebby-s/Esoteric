21a503afc8ce10bc11ac8a5d783087b78027f08f
ebby-s <31300178+ebby-s@users.noreply.github.com>
2018-12-02 18:43:00 +0800

    Update bf interpreter.py

--- bf interpreter.py
+++ bf interpreter.py
@@ -1,21 +1,22 @@
 global memory, pointer, in_memory, in_pointer
 memory = [0]
 pointer = 0
-in_memory = [0]
+in_memory = ["",""]
 in_pointer = 0
 program = open("program.txt").read().replace("\n","")
 
 def execute(program):        # Executes program
+    print(program)
     global memory, pointer, in_memory, in_pointer
     for i,char in enumerate(program):
-        #print(memory,char)
+        print(memory,char)
         if char == ">": move_pointer(1)
         elif char == "<": move_pointer(-1)
         elif char == "+": memory[pointer] += 1
-        elif char == "-": memory[pointer] -= 1
+        elif char == "-" and memory[pointer] > 0: memory[pointer] -= 1
         elif char == "[": run_loop(program,i)
         elif char == "]": """ends loop"""
-        elif char == ",": """inputs a character"""
+        elif char == ",": take_input()
         elif char == ".": print(chr(memory[pointer]))
         else: print(memory,pointer)
 
@@ -32,11 +33,22 @@ def run_loop(program,pos):    # Executes loop
     loop = ""
     pos += 1
     char = ""
-    while char != "]":
+    balance = 0
+    while char != "]" or balance != 0:
+        print(program,pos)
+        if char == "[": balance += 1
+        elif char == "]": balance -= 1
         loop += char
         char = program[pos]
         pos += 1
     while memory[pointer] > 1:
         execute(loop)
+
+def take_input():
+    global memory, pointer, in_memory, in_pointer
+    try:
+        memory[pointer] = ord(in_memory[in_pointer])
+        in_pointer += 1
+    except: memory[pointer] = 0
 
 execute(program)
